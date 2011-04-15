f57413126b00186730573c6b6ecc846edb607acc
zourtney <zourtney@gmail.com>
2011-04-15 02:28:19 +0000

    revisions revisions. They come cheap

--- scripts/globals.php
+++ scripts/globals.php
@@ -1,9 +1,11 @@
 <?php
 
 // Constants
-define('DEBUG', true);
+define('DEBUG', (strstr($_SERVER['SERVER_NAME'], 'localhost') !== false));
 
 if (DEBUG) {
+  // Put something obnoxious at the top of the page :-)
+  echo '<div style="margin: 0; padding: 1em; display: block; background: #ff7777;">Debug Mode -- localhost</div>';
   define('BASE_URL', 'http://localhost/gaslog/trunk/');
 }
 else {
