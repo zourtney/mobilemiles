d52b1a2c75da7eb6c1d88bba4926933c29c46d1e
zourtney <zourtney@gmail.com>
2011-07-26 21:16:10 +0000

    inevitable missing file

---
+++ templates/splash.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @copyright: Copyright 2011 randomland.net.
+ * @license:   Apache 2.0; see `license.txt`
+ * @author:    zourtney@randomland.net
+ * 
+ * Loading splash screen (keep this simple)
+ */
+?>
+
+<p>Loading...</p>
