44f61e530aca69de3eaa67f9ba4868cf82c1230d
zourtney <zourtney@gmail.com>
2011-08-08 18:40:12 -0700

    Shrunk subtitle font size

--- templates/ui.php
+++ templates/ui.php
@@ -63,7 +63,7 @@ function glHeader($v) {
     if (isset($v['settings']) && ($v['settings'] === false || strlen($v['settings']) < 1)) {
       echo 'style="display: none;"';
     } ?> data-icon="gear" class="ui-btn-right" data-iconpos="right" data-inline="true" href="<?php echo BASE_URL; ?>#settings">Settings</a>
-    <div data-role="navbar" class="subtitle" data-theme="d">
+    <div data-role="navbar" class="subtitle">
       <ul>
         <li class="subtitle"></li>
       </ul>
