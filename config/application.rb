37e379334c3f92a2efaec202064f954fd92dcee9
zourtney <zourtney@gmail.com>
2014-04-27 17:27:38 -0500

    Verifying CORS is cors'ing

--- config/application.rb
+++ config/application.rb
@@ -20,11 +20,11 @@ module Mobilemiles
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.middleware.use Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+    # config.middleware.use Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #   end
+    # end
   end
 end
