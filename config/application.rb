b6c85c9119686516b22945ab5f5ab80e736f8dad
zourtney <zourtney@gmail.com>
2014-05-16 23:49:00 -0500

    CORS-compatible PUT and DELETE http methods

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Mobilemiles
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
   end
